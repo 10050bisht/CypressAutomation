dfe2bf42e2f27f0623f73bd0416275f71c9e8fe9
himanshu10050 <hb047809@gmail.com>
2025-04-08 03:46:14 -0500

    Node module added

--- TopDriver_Cypress/cypress/e2e/Login.cy.js
+++ TopDriver_Cypress/cypress/e2e/Login.cy.js
@@ -1,31 +1,10 @@
 describe("TopDriver Login Test Cases", () => {
-  it.only("Test case:- invlid email format", () => {
-    cy.visit("https://staging.topdriverdev.com/login", {
-      waitUntil: "domcontentloaded",
-    });
-
-    cy.get('input[name="email"]').type("himanshu@$##yopmail.com"); // get locators with classname. type the text
-    cy.get("button[value='on']").click();
-    cy.get(".login-button").click(); // get locators with classname and click the button
-
-    cy.get(".error-message", { timeout: 5000 }) // Replace with actual selector (e.g., .tooltip, [data-cy="password-error"])
-      .should("be.visible")
-      .and(
-        "contain.text",
-        "A part following ‘@’ should not contain the symbol ‘#’"
-      );
-    // cy.get(".tooltip").should("be.visible").and("have.css", "display", "none");
-  });
-
   // Login with Blank Fields and verify the error message
   it("Test Case 1:- Login Blank fields", () => {
     cy.visit("https://staging.topdriverdev.com/login", {
       waitUntil: "domcontentloaded",
     });
 
-    // // cy.get('input[name="email"]').type("  "); // get locators with classname. type the text
-
-    // // cy.get('input[name="password"]').type("  ");
     cy.wait(2000);
     cy.get("button[value='on']").click();
 
