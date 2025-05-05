937061e8ba1815c04e8bbd1e04c205d9e2f1c167
himanshu10050 <hb047809@gmail.com>
2025-05-05 10:36:20 -0500

    topdriver api code

--- Cypress/cypress/e2e/Cypress_API/QueeryParams.cy.js
+++ Cypress/cypress/e2e/Cypress_API/QueeryParams.cy.js
@@ -9,6 +9,11 @@ describe("GET Requests", () => {
       expect(response.body.page).to.eq(2);
       expect(response.body.data.length).to.be.greaterThan(0);
       expect(response.body.data).have.length(6);
+
+      
+        expect(response.body.data[0]).have.property("id", "7");
+        expect(response.body.data(0)).have.property("first_name", "Michael");
+
     });
   });
 });
